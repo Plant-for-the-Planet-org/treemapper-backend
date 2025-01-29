b46b2513dc0fb8f8eb919c601c722a516ba864be
shyambhongle <shyambhongle@gmail.com>
2025-01-30 01:00:29 +0530

    minor CRUD

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { DATABASE_CONNECTION } from '../database/database-connection';
+import * as schema from './schema';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @Inject(DATABASE_CONNECTION)
+    private readonly database: NodePgDatabase<typeof schema>,
+  ) {}
+
+  async getUsers() {
+    return this.database.query.users.findMany({
+      with: { posts: true, profile: true },
+    });
+  }
+
+  async createUser(user: typeof schema.users.$inferInsert) {
+    await this.database.insert(schema.users).values(user);
+  }
+}
