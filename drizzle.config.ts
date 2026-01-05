7489d390bca27f65923d2e5de00b60ce6a121e56
shyambhongle <shyambhongle@gmail.com>
2026-01-05 11:37:56 +0530

    drizzle config fix

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,13 +6,9 @@ dotenv.config();
 export default {
   schema: './src/database/schema/index.ts',
   out: './drizzle/migrations',
-  dialect: 'postgresql', // This is the required parameter now, instead of 'driver: pg'
+  dialect: 'postgresql',
   dbCredentials: {
-    host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT) || 6543,
-    user: process.env.DB_USERNAME || 'postgres.famrkomiqrclihrrzcfu',
-    password: process.env.DB_PASSWORD || '[YOUR-PASSWORD]',
-    database: process.env.DB_NAME || 'postgres',
-    ssl: false,
+    url: process.env.DATABASE_URL!,
+    ssl: true,
   },
 } satisfies Config;
